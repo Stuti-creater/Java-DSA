4c93a3a4176f4b94eac9a2ad903d46f8f0f71fd6
Stuti-creater <stutiagrawal3030@gmail.com>
2026-08-27 22:47:35 +0530

    Time: 6 ms (72.13%) | Memory: 47 MB (77.87%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>>graph=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            graph.add(new ArrayList<>());
+        }
+        int[]indegree=new int[numCourses];
+        for(int[] prequisite:prerequisites){
+            int course=prequisite[0];
+            int pre=prequisite[1];
+            graph.get(pre).add(course);
+            indegree[course]++;
+
+
+        }
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+
+            int current = queue.poll();
+
+            count++;
+
+            
+            for (int next : graph.get(current)) {
+
+                indegree[next]--;
+
+                
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+        
+        return count == numCourses;
+
+    }
+}
